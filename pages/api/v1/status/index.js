17aba1ed7bd0bc025e0baa2e9e7e5962359af1e8
Marcos Magno <marcosmagnopb@gmail.com>
2025-12-01 13:57:24 -0300

    finish /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima de média" });
+  const updatedAt = new Date().toISOString();
+
+  const resultVersion = await database.query("SHOW server_version;");
+  const databaseVersionValue = resultVersion.rows[0].server_version;
+
+  const resultMaxConnections = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue = parseInt(
+    resultMaxConnections.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const resultUsedConnections = await database.query({
+    text: `SELECT count(*)::int used FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+  const databaseUsedConnectionsValue = resultUsedConnections.rows[0].used;
+
+  const dependencies = {
+    database: {
+      version: databaseVersionValue,
+      max_connections: databaseMaxConnectionsValue,
+      used_connections: databaseUsedConnectionsValue,
+    },
+  };
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status/");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const databaseInfo = responseBody.dependencies.database;
+
+  expect(typeof databaseInfo.version).toEqual("string");
+  expect(databaseInfo.version).toBe("16.0");
+
+  expect(typeof databaseInfo.max_connections).toEqual("number");
+  expect(databaseInfo.max_connections).toBe(100);
+
+  expect(typeof databaseInfo.used_connections).toEqual("number");
+  expect(databaseInfo.used_connections).toEqual(1);
 });
