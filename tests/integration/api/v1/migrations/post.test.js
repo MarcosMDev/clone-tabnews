db52450971be4cb9482a48795172ddd812d28713
Marcos Magno <marcosmagnopb@gmail.com>
2025-12-03 09:38:14 -0300

    add test for DELETE method on /api/v1/migrations endpoint to ensure proper error handling

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -27,3 +27,14 @@ test("POST to /api/v1/migrations should return 200", async () => {
   expect(Array.isArray(response2Body)).toBe(true);
   expect(response2Body.length).toBe(0);
 });
+
+test("DELETE to /api/v1/migrations should return 405", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations/", {
+    method: "DELETE",
+  });
+
+  const responseBody = await response.json();
+
+  expect(response.status).toBe(405);
+  expect(responseBody.error).toBe("Method DELETE not allowed");
+});
